113741ec3d44fd679167fb8c38d490b70551f387
Peter <113168584+prawstho@users.noreply.github.com>
2023-10-12 10:25:35 -0230

    CLI interface for token working

--- myapp.js
+++ myapp.js
@@ -14,11 +14,12 @@
  * 05 Oct 2023, PJR, altered for lecture prep
  *
  *************************/
-global.DEBUG = false;
+global.DEBUG = true;
 
 const fs = require("fs");
 const { initializeApp } = require('./init.js');
 const { configApp } = require('./config.js');
+const { tokenApp } = require('./token.js')
 
 const myArgs = process.argv.slice(2);
 if(DEBUG) if(myArgs.length >= 1) console.log('the myapp.args: ', myArgs);
